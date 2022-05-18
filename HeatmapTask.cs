615fce1bb3113249951df9f34a4822294bdea928
Zzigol <zigolenko@rambler.ru>
2022-05-18 21:03:55 +0300

    второе задание готово

--- HeatmapTask.cs
+++ HeatmapTask.cs
@@ -14,22 +14,13 @@ namespace Names
             for (var m = 0; m < 12; m++)
                 month[m] = (m + 1).ToString();
 
-           /* var days2 = new double[30];
-            for (var d = 0; d < 30; d++)
-                days2[d] = (d + 2);
-
-            var month2 = new double[12];
-            for (var m = 0; m < 12; m++)
-                month2[m] = (m + 1);*/
-
             double[,] twoDimensionalArray = new double[days.Length,month.Length];
             foreach (var imya in names)
             {
                 if ( imya.BirthDate.Day != 1)
                     twoDimensionalArray[imya.BirthDate.Day - 2, imya.BirthDate.Month - 1]++;
             }
-
-            //days.Length, month.Length
+            //
             return new HeatmapData(
                 "Пример карты интенсивностей",
                 twoDimensionalArray,

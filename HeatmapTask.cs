4ae87dc90b0eeeae0dead8acf92c16dc95f4fd65
Zzigol <zigolenko@rambler.ru>
2022-05-18 20:50:03 +0300

    решена вторая задача

--- HeatmapTask.cs
+++ HeatmapTask.cs
@@ -6,11 +6,35 @@ namespace Names
     {
         public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
         {
+            var days = new string[30];
+            for (var d = 0; d < 30; d++)
+                days[d] = (d + 2).ToString();
+
+            var month = new string[12];
+            for (var m = 0; m < 12; m++)
+                month[m] = (m + 1).ToString();
+
+           /* var days2 = new double[30];
+            for (var d = 0; d < 30; d++)
+                days2[d] = (d + 2);
+
+            var month2 = new double[12];
+            for (var m = 0; m < 12; m++)
+                month2[m] = (m + 1);*/
+
+            double[,] twoDimensionalArray = new double[days.Length,month.Length];
+            foreach (var imya in names)
+            {
+                if ( imya.BirthDate.Day != 1)
+                    twoDimensionalArray[imya.BirthDate.Day - 2, imya.BirthDate.Month - 1]++;
+            }
+
+            //days.Length, month.Length
             return new HeatmapData(
                 "Пример карты интенсивностей",
-                new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-                new[] { "a", "b", "c", "d" }, 
-                new[] { "X", "Y", "Z" });
+                twoDimensionalArray,
+                days,
+                month);
         }
     }
 }
